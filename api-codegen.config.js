dc7d0b36d12142e1e32305df971b92625b2e10a0
Alexander <ao_socz@list.ru>
2025-08-15 15:33:23 +0400

    init

--- api-codegen.config.js
+++ api-codegen.config.js
@@ -1,6 +1,6 @@
 const config = {
   schemaFile: 'openapi.yaml',        // твой YAML или JSON со схемой
-  apiFile: './src/api/baseApi.ts',  // файл, где лежит пустой createApi, создайте там baseApi и экспортните
+  apiFile: './src/api/baseApi.ts',  // файл, где лежит пустой createApi без endpoints, тот api и поключаешь в редьюсеры
   outputFile: './src/api/generatedApi.ts', // куда писать сгенерённый код
   exportName: 'generatedApi',        // имя экспортируемого API
   hooks: true,                       // генерировать хуки (useXxxQuery)
